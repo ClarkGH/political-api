07780df2e77ba4611f2a1b5738ab5df5935d569d
clarkgh <demolisho@hotmail.com>
2017-09-03 06:52:56 -0700

    added delete spec

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -98,6 +98,10 @@ RSpec.describe 'Todos API', type :request do
 
   # Test suide for DELETE /todos:id
   describe 'DELETE /todos:id' do
+    before { delete "/todos/#{todo_id}" }
 
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
   end
 end
