e62a37108832f8beaa3df1a746782c9c398fa5b5
clarkgh <demolisho@hotmail.com>
2017-09-03 08:21:41 -0700

    fixed syntax errors, specs failing as intended

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Todos API', type :request do
+RSpec.describe 'Todos API', type: :request do
   # Init test data
   let!(:todos) { create_list(:todo, 10) }
   let(:todo_it) { todos.first.id }
@@ -13,11 +13,11 @@ RSpec.describe 'Todos API', type :request do
     it 'returns todos' do
       # Note 'json' is a custom helper to parse JSON responses
       expect(json).not_to be_empty
-      expect(json.size)to eq(10)
+      expect(json.size).to eq(10)
     end
 
     it 'returns status code 200' do
-      expect(response) to have_http_status(200)
+      expect(response).to have_http_status(200)
     end
   end
 
